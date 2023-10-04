ddca00a8aec3d7a13334db09c7175178e96b709e
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-10-04 11:24:29 +0000

    more queries

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,12 +1,5 @@
--- USE hbtn_0d_usa;
--- SELECT * FROM states;
--- ALTER TABLE cities;
--- DROP COLUMN state_id;
-SELECT * FROM cities WHERE state_id = 1 AND name = 'Carlifornia';
-
-
--- SELECT * FROM cities
--- WHERE state_id = (
---     SELECT id FROM states
---     WHERE name = 'California'
--- )
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+  AND states.name = 'California'
+ORDER BY cities.id ASC;
