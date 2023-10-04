8993088be49a1aa1ad4fafb9459bb94ea6802fd0
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-10-04 10:39:01 +0000

    more queries

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
+-- db and user with only select permission
 CREATE SCHEMA IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-SET PASSWORD FOR 'user_0d_2'@'localhost'='user_0d_2_pwd';
+SET PASSWORD FOR 'user_0d_2s'@'localhost'='user_0d_2_pwd';
 GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 -- SHOW GRANTS FOR 'user_0d_2'@'localhost';
