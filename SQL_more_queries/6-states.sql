563ae1d7b286571f320f6fc5497a4a3f4482d462
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-10-04 10:42:32 +0000

    more queries

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
+-- States
 CREATE SCHEMA IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states
--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,3 +1,4 @@
+-- Cities
 CREATE SCHEMA IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities
