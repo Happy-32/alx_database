db3b350e7d74a9165e4b9c99086accc7654c4283
Happy-32 <yahayaenmaariandrew@gmain.com>
2023-10-04 11:29:38 +0000

    more queries

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- USE hbtn_0d_usa;
 -- SELECT * FROM states;
-SELECT cities.id, cities.name
+SELECT cities.id, cities.name, states.name
 FROM cities, states
 WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
